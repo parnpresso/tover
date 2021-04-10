679c37c27983060fb2c20c8f58b16d6e5f7b7ab7
Parn Boonyamanee <parnpresso@gmail.com>
2021-04-10 21:01:08 +0700

    ✨ Change text color :crown:

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 
 const App = () => {
   const [cardCoverColor, setCardCoverColor] = useState<string>('#000000');
+  const [cardTextColor, setCardTextColor] = useState<string>('#000000');
   const [cardMessage, setCardMessage] = useState<string>('APRIL 🦉');
+  const [copyButtonMessage, setCopyButtonMessage] = useState<string>('Copy');
 
   useEffect(() => {
     randomCardColor();
@@ -13,32 +15,58 @@ const App = () => {
     setCardCoverColor(randomColor);
   }
 
-  const downloadCover = () => {
-    console.log('Download');
+  const changeTextColor = () => {
+    if (cardTextColor === '#000000') setCardTextColor('#FFFFFF');
+    else setCardTextColor('#000000');
+  }
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText('test');
+    setCopyButtonMessage('Image copied!');
+    setTimeout(() => setCopyButtonMessage('Copy'), 2000);
   }
 
   return (
     <div className="flex flex-col items-center">
       <div className="mt-12 text-gray-400 font-bold text-lg">Tover : Trello card cover generator</div>
 
-      <div className="mt-20 w-64 shadow-lg rounded-md mb-8">
+      <div className="mt-20 w-64 lg:w-80 shadow-lg rounded-md mb-12">
         <div className="flex items-center justify-center h-20 rounded-tl-md rounded-tr-md" style={{ backgroundColor: cardCoverColor }}>
-          <h1 className="text-4xl font-bold">{ cardMessage }</h1>
+          <h1 className="text-4xl font-bold" style={{ color: cardTextColor }}>{ cardMessage }</h1>
         </div>
         <div className="p-2 pb-4 rounded-bl-md rounded-br-md">
-          <h3>Trello card mockup</h3>
+          <h3>This is a card's title</h3>
         </div>
       </div>
 
-      <div className="flex items-start justify-start">
+      <div className="flex flex-col mb-4">
         <input
-          className="shadow-md rounded-md p-2 bg-gray-50 focus:outline-none mr-2"
+          className="p-2 w-48 rounded-md border-2 border-black text-black focus:outline-none mb-2"
           type="text"
           value={cardMessage}
           onChange={(event) => setCardMessage(event.target.value)}
         />
-        <button className="p-2 rounded-md shadow-md bg-gray-50 focus:outline-none mr-2" onClick={randomCardColor}>🔄</button>
-        <button className="p-2 rounded-md shadow-md bg-gray-50 focus:outline-none" onClick={downloadCover}>⬇️</button>
+
+        <button
+          className="p-2 w-48 rounded-md border-2 border-black text-sm text-black font-bold focus:outline-none mb-2"
+          onClick={randomCardColor}
+        >
+          Background color
+        </button>
+
+        <button
+          className="p-2 w-48 rounded-md border-2 border-black text-sm text-black font-bold focus:outline-none mb-8"
+          onClick={changeTextColor}
+        >
+          Text color
+        </button>
+
+        <button
+          className="p-2 w-48 rounded-md border-2 border-black text-black font-bold focus:outline-none"
+          onClick={copyToClipboard}
+        >
+          {copyButtonMessage}
+        </button>
       </div>
     </div>
   );
